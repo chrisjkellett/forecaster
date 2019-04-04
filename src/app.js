fa775f5401dc8ce7818fe0d9c70bd92295ded53c
chrisjkellett <cjkellett@hotmail.co.uk>
2019-04-04 20:39:42 +0200

    connect calls to api to my api

--- src/app.js
+++ src/app.js
@@ -1,20 +1,35 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const sampleResponse = require('./sampleJSON.json');
+const mapboxAPI = require('./utils/mapbox');
+const darkskyAPI = require('./utils/darksky');
 
 app.use(express.static(path.join(__dirname, './public')));
 
-app.get('/weather', (req, res) => {
-  if(req.query.location){
-    sampleResponse.data.location = req.query.location;
-    return res.json(sampleResponse);
+function throwError(msg){
+  return {
+    error: msg || 'no results found'
   }
+}
 
-  res.json({
-    error: 'location must be provided'
-  })
-
+app.get('/weather', (req, res) => {
+  if(!req.query.location){
+    return res.json(throwError('a location must be provided')) 
+  }
+  mapboxAPI(req.query.location, (error, mapboxData) => {
+    if(error){
+      return res.json(throwError())
+    }
+    darkskyAPI(mapboxData, (error, {data}, location) => {
+      if(error){
+        return res.json(throwError('something went wrong'));
+      }
+      res.json({
+        temperature: Math.round(data.currently.temperature),
+        location: location
+      })
+    });
+  });
 });
 
 app.listen(3000);
--- src/utils/darksky.js
+++ src/utils/darksky.js
@@ -1,13 +1,12 @@
 const axios = require('axios');
 const {fetchDarkskyURL} = require('./urls');
-const printForecast = require('./printForecast');
 
 module.exports = ({latitude, longitude, location}, callback) => {
   axios.get(fetchDarkskyURL(latitude, longitude))
     .then((response) => {
-      printForecast(response, location);
+      callback(undefined, response, location);
     })
     .catch((error) => {
-      console.log(error);
+      callback(error);
     });
 }
